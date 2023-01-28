d6f996775f854b1b98cf7a914928ea6ad4d4231b
mukaruernest <mukaruernesto@gmail.com>
2023-01-28 08:27:35 +0300

    task listener submision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form')
+  const addClick = document.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let taskName = e.target.new_task_description.value
+    let p = document.createElement('p')
+    let button = document.createElement('button')
+    button.addEventListener('click', handleDelete)
+    button.textContent = 'x'
+    p.textContent=taskName
+    p.appendChild(button)
+    document.querySelector('#list').appendChild(p)
+    form.reset()
+  })
+ 
+}); 
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
